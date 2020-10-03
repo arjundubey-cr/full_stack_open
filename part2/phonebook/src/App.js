55408c4746614700b8a38e59a54c6d1edf21b2d3
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-04 01:28:03 +0530

    added phonebook_2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,50 +1,56 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  
-  const [ persons, setPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  
-  const Person=(props)=>{
 
-    const personname = props.person.name;
-    return(
-      <div>
-        {personname}
-      </div>
-    )
-  }
-  const handleNameChange = (event)=>{
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+
+  // const Person = (props) => {
+  //   const personname = props.person.name;
+  //   return (
+  //     <div>
+  //       {personname}
+  //     </div>
+  //   )
+  // }
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-  const createPerson=()=>{
-    const person={
+  const createPerson = () => {
+    const person = {
       name: newName,
     };
     return person;
   }
-  
-  const handleSubmit=(event)=>{
+
+  const handleSubmit = (event) => {
     event.preventDefault()
+    const checkDuplicate=persons.find((p)=>p.name===newName);
+    if (typeof(checkDuplicate) != "undefined"){
+      window.alert(`${checkDuplicate.name} is already in the list`)
+    }
+    else{
     const person = createPerson()
     setPersons(persons.concat(person))
+    window.alert(`${person.name} has been added to the list`)
+    }
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button onClick={handleSubmit}>add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(
-          person => <div key={Math.random()*10}>{person.name}</div>)
-          }
+          person => <div key={Math.random() * 10}>{person.name}</div>)
+        }
       </div>
     </div>
   )
