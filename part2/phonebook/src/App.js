d32e0aead467359003b0a48d96eac00917f8d877
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-04 21:53:06 +0530

    added phonebook2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-
+  const [newPhoneNum, setPhoneNum]=useState('')
   // const Person = (props) => {
   //   const personname = props.person.name;
   //   return (
@@ -16,9 +16,13 @@ const App = () => {
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  const handlePhoneChange = (event) => {
+    setPhoneNum(event.target.value)
+  }
   const createPerson = () => {
     const person = {
       name: newName,
+      number: newPhoneNum,
     };
     return person;
   }
@@ -32,7 +36,7 @@ const App = () => {
     else{
     const person = createPerson()
     setPersons(persons.concat(person))
-    window.alert(`${person.name} has been added to the list`)
+    // window.alert(`${person.name} has been added to the list`)
     }
   }
   return (
@@ -43,13 +47,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          phonenumber: <input value={newPhoneNum} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(
-          person => <div key={Math.random() * 10}>{person.name}</div>)
+          person => <div key={Math.random() * 10}>{person.name} {person.number}</div>)
         }
       </div>
     </div>
