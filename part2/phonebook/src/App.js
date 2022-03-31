1657a681e26aed169ba5d8e39787d94c9abdd7f7
Arjun Dubey <arjundubeycr@gmail.com>
2022-03-31 06:50:22 +0530

    dependency management

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
 const App = () => {
-
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newPhoneNum, setPhoneNum]=useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ])
+  const [newName, setNewName] = useState("")
+  const [newPhoneNum, setPhoneNum] = useState("")
   // const Person = (props) => {
   //   const personname = props.person.name;
   //   return (
@@ -23,20 +27,19 @@ const App = () => {
     const person = {
       name: newName,
       number: newPhoneNum,
-    };
-    return person;
+    }
+    return person
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const checkDuplicate=persons.find((p)=>p.name===newName);
-    if (typeof(checkDuplicate) != "undefined"){
+    const checkDuplicate = persons.find((p) => p.name === newName)
+    if (typeof checkDuplicate != "undefined") {
       window.alert(`${checkDuplicate.name} is already in the list`)
-    }
-    else{
-    const person = createPerson()
-    setPersons(persons.concat(person))
-    // window.alert(`${person.name} has been added to the list`)
+    } else {
+      const person = createPerson()
+      setPersons(persons.concat(person))
+      // window.alert(`${person.name} has been added to the list`)
     }
   }
   return (
@@ -50,14 +53,18 @@ const App = () => {
           phonenumber: <input value={newPhoneNum} onChange={handlePhoneChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleSubmit}>add</button>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(
-          person => <div key={Math.random() * 10}>{person.name} {person.number}</div>)
-        }
+        {persons.map((person) => (
+          <div key={Math.random() * 10}>
+            {person.name} {person.number}
+          </div>
+        ))}
       </div>
     </div>
   )
